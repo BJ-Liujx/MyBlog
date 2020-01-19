940c67ce5e321ec2c56bb2754493621fd629d8ef
BJ_liujx <BJ_liujx@163.com>
2020-01-19 16:48:58 +0800

    1

--- web/src/main/java/com/liujx/web/WebApplication.java
+++ web/src/main/java/com/liujx/web/WebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.liujx.controller")
+@ComponentScan(basePackages = "com.liujx")
 public class WebApplication {
 
     public static void main(String[] args) {
